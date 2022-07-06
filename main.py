fcfc254dd8923635f1538e97149e664d87cdb69d
Squarehead365 <oleksiigovera@gmail.com>
2022-07-06 12:35:24 +0300

    test gui commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter.tix import ButtonBox
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
+
+class ButtonBox(BoxLayout):
+    pass
+    
+
+
+class TheLabApp(App):
+    pass
+
+TheLabApp().run()
